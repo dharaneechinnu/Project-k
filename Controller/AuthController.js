86449072ac54f16496030efcfd6fd533303986b2
Your Name <your.email@example.com>
2024-09-13 23:06:51 +0530

    backen

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -88,7 +88,7 @@ const register = async (req, res) => {
   
       res.status(200).json({ message: 'User registered successfully' });
     } catch (error) {
-      console.error('Error registering user:', error);
+      console.error('Error registering users:', error);
       res.status(500).json({ message: 'Internal server error' });
     }
   };
